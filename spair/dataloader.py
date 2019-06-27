major bug fix on gradient propagation, now decoder seems to be working

spair/dataloader.py
@@ -1,7 +1,7 @@
 
 import h5py
 import numpy as np
-
+from spair import config as cfg
 import torch
 from torch.utils import data
 import cv2
@@ -14,13 +14,14 @@ class SimpleScatteredMNISTDataset(torch.utils.data.Dataset):
         self.episode = None
 
         static_img = self.dataset[9, ...]
-        self.static_img = cv2.resize(static_img, dsize=(4,4))
+        img_size = cfg.INPUT_IMAGE_SHAPE[-1]
+        self.static_img = cv2.resize(static_img, dsize=(img_size,img_size))
 
     def __getitem__(self, index):
         ret = []
 
-        # obs = self.dataset[9, ...] # TODO index fixed to 0
-        obs = self.static_img
+        obs = self.dataset[9, ...] # TODO index fixed to 0
+        # obs = self.static_img
         # obs = np.zeros_like(obs)
         obs = obs[..., None]  # Add channel dimension
         ret = np.moveaxis(obs, -1, 0)  # move from (x, y, c) to (c, x, y)

spair/debug_tools.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 
 from spair import config as cfg
-GRID_SIZE = 1 # TODO Originally 11
+GRID_SIZE = 11
 def plot_torch_image_in_pyplot( out:torch.Tensor, inp:torch.Tensor = None, batch_n=0):
     ''' For visualizing torch images in matplotlib '''
     torch_img = out[batch_n, ...]
@@ -55,8 +55,8 @@ def plot_prerender_components(obj_vec, z_pres, z_depth, bounding_box, input_imag
     # obj_vec = obj_vec.view(32, 11, 11, 28, 28, 3)
     obj_vec = torch2npy(obj_vec, reshape=True)
     obj_vec = obj_vec[0, ...]
-    # obj_vec = np.concatenate(obj_vec, axis=-3) # concat h
-    # obj_vec = np.concatenate(obj_vec, axis=-2) # concat w
+    obj_vec = np.concatenate(obj_vec, axis=-3) # concat h
+    obj_vec = np.concatenate(obj_vec, axis=-2) # concat w
     # z_pres = z_pres.view(32, 11, 11, 1)
     z_pres = torch2npy(z_pres, reshape=True)
     z_depth = torch2npy(z_depth, reshape=True)
@@ -174,14 +174,11 @@ def _plot_bounding_boxes(title, bbox, original_image, gridspec, fig):
     ax = fig.add_subplot(gridspec)
     ax.imshow(original_image, cmap='gray', vmin=0, vmax=1)
     #ptchs = []
-    # for rows in bbox:
-    #     for cols in rows:
-    #         x, y, w, h = cols
-    x, y, w, h = bbox
-    patch = patches.Rectangle([x,y], w, h, facecolor='none', edgecolor='r', linewidth=1)
-    #ptchs.append(patch)
-    ax.add_patch(patch)
-
+    for rows in bbox:
+        for cols in rows:
+            x, y, w, h = cols
+            patch = patches.Rectangle([x,y], w, h, facecolor='none', edgecolor='r', linewidth=1)
+            ax.add_patch(patch)
 
     # ax.add_collection(PatchCollection(ptchs, facecolors='none', edgecolors='r', linewidths=1))
     ax.set_xticks([])

spair/models.py
@@ -49,7 +49,7 @@ class SPAIR(nn.Module):
         context_mat = {}
 
         z_where = torch.empty(self.batch_size, 4, H, W).to(self.device) # 4 = xt, yt, xs, ys
-        self.z_attr = torch.empty(self.batch_size, cfg.N_ATTRIBUTES, H, W,).to(self.device)
+        z_attr = torch.empty(self.batch_size, cfg.N_ATTRIBUTES, H, W,).to(self.device)
         z_depth = torch.empty(self.batch_size, 1, H, W).to(self.device)
         z_pres = torch.empty(self.batch_size, 1, H, W).to(self.device)
         z_pres_prob = torch.empty(self.batch_size, 1, H, W).to(self.device)
@@ -81,7 +81,7 @@ class SPAIR(nn.Module):
             input_glimpses, attr_latent_var = self._encode_attr(x, normalized_box)
             attr_mean, attr_std = latent_to_mean_std(attr_latent_var)
             attr = self._sample_z(attr_mean, attr_std, 'attr', (h, w))
-            self.z_attr[:, :, h, w, ] = attr
+            z_attr[:, :, h, w, ] = attr
 
             # TODO DELETE ME
             # for name, t in self.object_decoder.named_parameters():
@@ -90,7 +90,7 @@ class SPAIR(nn.Module):
             #             lambda grad: debug_tools.decoder_grad_hook(grad, self.writer, self.global_step))
 
                     # t.register_hook(lambda grad: self.writer.add_histogram('decoder_individual_grad/%d' % i , grad, self.global_step))
-            # debug_cropped_images[..., h, w] = input_glimpses
+            debug_cropped_images[..., h, w] = input_glimpses
             # self.writer.add_histogram('z_attr/%d_%d' % (h, w), z_attr[0, :, h, w, ], self.global_step)
             # TODO END
 
@@ -126,8 +126,8 @@ class SPAIR(nn.Module):
 
 
         # TODO Delete me
-        # debug_tools.plot_cropped_input_images(debug_cropped_images, self.writer, self.global_step)
-        debug_tools.plot_objet_attr_latent_representation(self.z_attr, self.writer, self.global_step)
+        debug_tools.plot_cropped_input_images(debug_cropped_images, self.writer, self.global_step)
+        debug_tools.plot_objet_attr_latent_representation(z_attr, self.writer, self.global_step)
         # TODO END
         # Merge dist param, we have to use loop or autograd might not work
         for dist_name, dist_params in self.dist_param.items():
@@ -140,7 +140,7 @@ class SPAIR(nn.Module):
 
         kl_loss = self._compute_KL(z_pres, z_pres_prob)
 
-        recon_x = self._render(self.z_attr, z_where, z_depth, z_pres, x)
+        recon_x = self._render(z_attr, z_where, z_depth, z_pres, x)
 
         loss = self._build_loss(x, recon_x, kl_loss)
 
@@ -572,7 +572,7 @@ class SPAIR(nn.Module):
     def _build_loss(self, x, recon_x, kl):
         print('============ Losses =============')
         # Reconstruction loss
-        recon_loss = F.binary_cross_entropy(recon_x, x,) # recon loss
+        recon_loss = F.binary_cross_entropy(recon_x, x, reduction='sum')
         self.writer.add_scalar('losses/reconst', recon_loss, self.global_step)
         print('Reconstruction loss:', '{:.4f}'.format(recon_loss.item()))
         # KL loss with Beta factor