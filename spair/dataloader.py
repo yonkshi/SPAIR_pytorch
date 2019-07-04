bug fixes and added average precision metric

spair/dataloader.py
@@ -10,24 +10,28 @@ import cv2
 class SimpleScatteredMNISTDataset(torch.utils.data.Dataset):
     def __init__(self, in_file):
         super().__init__()
-        self.dataset = h5py.File(in_file, 'r')['test']
+        self.dataset = h5py.File(in_file, 'r')['train/full']
         self.episode = None
 
-        static_img = self.dataset[9, ...]
-        img_size = cfg.INPUT_IMAGE_SHAPE[-1]
-        self.static_img = cv2.resize(static_img, dsize=(img_size,img_size))
+        # static_img = self.dataset[9, ...]
+        # img_size = cfg.INPUT_IMAGE_SHAPE[-1]
+        # self.static_img = cv2.resize(static_img, dsize=(img_size,img_size))
 
     def __getitem__(self, index):
         ret = []
 
-        obs = self.dataset[index, ...] # TODO index fixed to 0
+        obs = self.dataset['image'][index, ...] # TODO index fixed to 0
         # obs = self.static_img
         # obs = np.zeros_like(obs)
         obs = obs[..., None]  # Add channel dimension
-        ret = np.moveaxis(obs, -1, 0)  # move from (x, y, c) to (c, x, y)
+        image = np.moveaxis(obs, -1, 0)  # move from (x, y, c) to (c, x, y)
 
-        return ret
+        bbox = self.dataset['bbox'][index, ...]  # TODO index fixed to 0
+
+        digit_count = self.dataset['digit_count'][index, ...]
+
+        return image, bbox, digit_count
 
     def __len__(self):
-        return self.dataset.shape[0]
+        return self.dataset['image'].shape[0]
 

spair/debug_tools.py
@@ -87,13 +87,14 @@ def plot_prerender_components(obj_vec, z_pres, z_depth, bounding_box, input_imag
 
     # Bounding Box
     bbox = bounding_box[0, ...] * cfg.INPUT_IMAGE_SHAPE[-2] # image size
-    _plot_bounding_boxes('bounding boxes', bbox, input_image, gs[1,0], fig)
+    presence = z_pres[0, ...]
+    _plot_bounding_boxes('bounding boxes', bbox, input_image, presence,  gs[1,0], fig)
 
     # depth (heatmap)
     depth = z_depth[0,...]
     _plot_heatmap('z_depth', depth, gs[1, 1], fig, cmap='autumn')
     # Presence (heatmap)
-    presence = z_pres[0,...]
+
     _plot_heatmap('z_presence', presence, gs[1, 2], fig, cmap='winter')
 
     if cfg.IS_LOCAL:
@@ -170,17 +171,23 @@ def _plot_image(title, data, gridspec, fig):
     if title:
         ax.set_title(title)
 
-def _plot_bounding_boxes(title, bbox, original_image, gridspec, fig):
+def _plot_bounding_boxes(title, bbox, original_image, z_pres, gridspec, fig):
 
     ax = fig.add_subplot(gridspec)
     ax.imshow(original_image, cmap='gray', vmin=0, vmax=1)
     #ptchs = []
-    for rows in bbox:
-        for cols in rows:
-            x, y, w, h = cols
+    H, W, _ = bbox.shape
+    for i in range(H):
+        for j in range(W):
+            x, y, w, h = bbox[i,j]
+            pres = np.clip(z_pres[i, j], 0.2, 1)
+            border_color = (1,0,0,pres) if pres > 0.5 else (0,0,1, pres)# red box if > 0.5, otherwise blue
+
+            # Green box: ground truth, red box: inferrence, blue box: disabled inferrence
+
             x -= w/2
             y -= h/2
-            patch = patches.Rectangle([x,y], w, h, facecolor='none', edgecolor='r', linewidth=1)
+            patch = patches.Rectangle([x,y], w, h, facecolor='none', edgecolor=border_color, linewidth=1)
             ax.add_patch(patch)
 
     # ax.add_collection(PatchCollection(ptchs, facecolors='none', edgecolors='r', linewidths=1))
@@ -234,4 +241,35 @@ def z_attr_grad_hook(grad, writer, step):
         print('')
     else:
         writer.add_figure('grad_visualization/z_attr', fig, step)
+
+def nan_hunter(name, **kwargs):
+
+    nan_detected = False
+    tensors = {}
+    non_tensors = {}
+    for name, value in kwargs.items():
+        if isinstance(value, torch.Tensor):
+            tensors[name] = value
+            if torch.isnan(value).sum() > 0:
+                nan_detected = True
+        else:
+            non_tensors[name] = value
+
+    if not nan_detected: return
+
+
+    print('======== NAN DETECTED in %s =======' % name)
+
+    for name, value in non_tensors.items():
+        print(name, ":", value)
+
+    for name, tensor in tensors.items():
+        print(name, ':\n', tensor)
+
+    print('======== END OF NAN DETECTED =======')
+
+    raise AssertionError('NAN Detected by Nan detector')
+
+
+
 

spair/models.py
@@ -62,28 +62,27 @@ class SPAIR(nn.Module):
         # s = torch.cuda.Stream()
         # # # Iterate through each grid cell and bounding boxes for that cell
         # with torch.cuda.stream(s):
-
-        # TODO DELETE ME:
-        # debug_cropped_images = torch.empty(self.batch_size, 1, *cfg.OBJECT_SHAPE, H, W)
-        debug_tools.benchmark('feature extraction')
-        # TODO DELETE ME END
+        debug_tools.nan_hunter('Before Main Loop', edge_element = edge_element, backbone_feature = feat, global_step = self.global_step,)
         for h, w in itertools.product(range(H), range(W)):
 
             # feature vec for each cell in the grid
             cell_feat = feat[:, :, h, w]
 
             context = self._get_sequential_context(context_mat, h,w, edge_element)
-            # --- box ---
+
+            # --- z_where ---
             layer_inp = torch.cat((cell_feat, context), dim=-1)
             rep_input, passthru_features = self.box_network(layer_inp)
             box, normalized_box = self._build_box(rep_input, h, w)
             z_where[:, :, h, w,] = normalized_box
-            # --- attr ---
+
+            # --- z_what ---
             input_glimpses, attr_latent_var = self._encode_attr(x, normalized_box)
             attr_mean, attr_std = latent_to_mean_std(attr_latent_var)
             attr = self._sample_z(attr_mean, attr_std, 'attr', (h, w))
             z_attr[:, :, h, w, ] = attr
-            # --- depth ---
+
+            # --- z_depth ---
             layer_inp = torch.cat([cell_feat, context, passthru_features, box, attr], dim=1)
 
             depth_latent, passthru_features = self.z_network(layer_inp)
@@ -95,7 +94,7 @@ class SPAIR(nn.Module):
             depth = 4 * clamped_sigmoid(depth_logits)
             z_depth[:,:, h, w] = depth
 
-            # --- presence ---
+            # --- z_presence ---
             layer_inp = torch.cat([cell_feat, context, passthru_features, box, attr, depth], dim=1)
             pres_logit = self.obj_network(layer_inp)
             obj_pres, obj_pres_prob = self._build_obj_pres(pres_logit)
@@ -104,30 +103,29 @@ class SPAIR(nn.Module):
             z_pres_prob[:, :, h, w] = obj_pres_prob
             context_mat[(h,w)] = torch.cat((box, attr, depth, obj_pres), dim=-1)
 
+            debug_tools.nan_hunter('Main Loop',
+                                    global_step = self.global_step,
+                                    grid_h=h,
+                                    grid_w = w,
+                                   context=context,
+                                   normalized_box = normalized_box,
+                                   attr = attr,
+                                   depth=depth,
+                                   presence = obj_pres
+                                   )
 
-        # TODO Delete me
-        debug_tools.benchmark('main loop')
-        # debug_tools.plot_cropped_input_images(debug_cropped_images, self.writer, self.global_step)
-        # debug_tools.plot_objet_attr_latent_representation(z_attr, self.writer, self.global_step)
-        # TODO END
+
         # Merge dist param, we have to use loop or autograd might not work
         for dist_name, dist_params in self.dist_param.items():
             means = self.dist_param[dist_name]['mean']
             sigmas = self.dist_param[dist_name]['sigma']
             self.dist[dist_name] = Normal(loc=means, scale=sigmas)
-        # if torch.isnan(z_pres).sum(): print('!!! !!! there is nan in z_pres')
-        # if torch.isnan(z_pres_prob).sum(): print('!!! !!! there is nan in z_pres_prob')
 
-        debug_tools.benchmark('merge distributions')
         kl_loss = self._compute_KL(z_pres, z_pres_prob)
-        debug_tools.benchmark('KL computation')
         recon_x = self._render(z_attr, z_where, z_depth, z_pres, x)
-        debug_tools.benchmark('rendering image')
         loss = self._build_loss(x, recon_x, kl_loss)
-        debug_tools.benchmark('computing loss')
-        # self._debug_logging(z_where, z_attr, z_pres, z_depth)
 
-        return loss, recon_x, z_where
+        return loss, recon_x, z_where, z_pres
 
     def _build_networks(self):
 
@@ -212,6 +210,9 @@ class SPAIR(nn.Module):
 
             prob = z_pres_prob[:, :, h, w]
 
+            # TODO This is for testing uniform dist
+            # p_z = torch.ones_like(prob) / HW
+
             # Bernoulli KL
             # note to self: May need to use safe log to prevent NaN
             _obj_kl = (
@@ -228,7 +229,6 @@ class SPAIR(nn.Module):
             mult = sample * p_z_given_Cz + (1-sample) * (1-p_z_given_Cz)
 
             # update count distribution
-            # FIXME why multiplying mult
             count_distribution1 = mult * count_distribution
             normalizer = count_distribution1.sum(dim=1, keepdim=True).clamp(min=1e-6)
             # why clip normalizer?
@@ -236,31 +236,23 @@ class SPAIR(nn.Module):
             count_distribution = count_distribution1 / normalizer
 
             # Test underflow issues
-            isnan = torch.isnan(obj_kl).sum()
-            isneg = (count_distribution < 0).float().sum()
-            if isnan > 0 or isneg > 0:
-                print('\n\n\n\t------------------- NAN OCCURED %d-----------------\n' % i)
-                print('is neg', isneg)
-                print('is nan', isnan)
-                print('_obj_kl:\n', _obj_kl)
-                print('\np_z:\n', p_z)
-                # print('\nprob:\n', prob, 'hw', h,w)
-                print('\nprob:\n', prob)
-                # print('\nz_pres_prob max & min:\n', z_pres_prob.max(), ' min ', z_pres_prob.min())
-                print('\ncount_distribution:\n', count_distribution)
-                # print('\ncount_so_far:\n', count_so_far)
-                # print('\nHW, i:\n', HW, i)
-                # print('\nsample:\n', sample)
-                print('\np_z_given_Cz:\n', p_z_given_Cz)
-                # print('\nmult:\n', mult)
-                raise AssertionError('Yo you dun goof')
+
+            debug_tools.nan_hunter('KL Divergence',
+                                   global_step = self.global_step,
+                                   grid_location = (h,w),
+                                   _obj_kl = _obj_kl,
+                                   p_z = p_z,
+                                   prob = prob,
+                                   count_distribution = count_distribution,
+                                   p_z_given_cz = p_z_given_Cz,
+                                   )
+
             count_so_far += sample
 
             i += 1
 
 
         KL['pres_dist'] = obj_kl
-
 
         return KL
 
@@ -489,8 +481,8 @@ class SPAIR(nn.Module):
         object_logits[:, :, :, -1] *= cfg.ALPHA_LOGIT_SCALE
         object_logits[:, :, :, -1] += cfg.ALPHA_LOGIT_BIAS
         # TODO DELETE ME
-        z_attr.register_hook(lambda grad: debug_tools.z_attr_grad_hook(grad, self.writer, self.global_step))
-        object_logits.register_hook(lambda grad: debug_tools.decoder_output_grad_hook(grad, self.writer, self.global_step))
+        # z_attr.register_hook(lambda grad: debug_tools.z_attr_grad_hook(grad, self.writer, self.global_step))
+        # object_logits.register_hook(lambda grad: debug_tools.decoder_output_grad_hook(grad, self.writer, self.global_step))
         # TODO END DELETE ME
         objects = clamped_sigmoid(object_logits, use_analytical=True)
         objects = objects.view(-1, px, px, input_chan_w_alpha)
@@ -667,6 +659,41 @@ class ObjectConvDecoder(nn.Module):
 
         return self.linear(conv_out_flat)
 
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_dim, activation):
+        super(Self_Attn, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)  #
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B X CX(N)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B X C x (*W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # BX (N) X (N)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
 
 
 

train.py
@@ -13,6 +13,8 @@ from spair.models import SPAIR
 from spair import config as cfg
 from spair.dataloader import SimpleScatteredMNISTDataset
 from spair import debug_tools
+from spair import metric
+
 
 dt = datetime.today().strftime('%b-%d') + '-' + generate_slug(2)
 writer = SummaryWriter('logs_v2/%s' % dt)
@@ -31,7 +33,7 @@ def train():
     image_shape = cfg.INPUT_IMAGE_SHAPE
 
     # Test image setup
-    data = SimpleScatteredMNISTDataset('spair/data/scattered_mnist_14x14.hdf5')
+    data = SimpleScatteredMNISTDataset('spair/data/scattered_mnist_128x128_obj14x14.hdf5')
     torch.manual_seed(3)
 
     spair_net = SPAIR(image_shape, writer, DEVICE).to(DEVICE)
@@ -45,23 +47,37 @@ def train():
                                             batch_size=cfg.BATCH_SIZE,
                                            pin_memory=True,
                                            num_workers= 1,
+                                           drop_last = True,
                                            )
         for batch_idx, batch in enumerate(dataloader):
+            x_image, y_bbox, y_digit_count = batch
             iteration = epoch * len(dataloader) + batch_idx
-            batch = batch.to(DEVICE)
+
+            x_image = x_image.to(DEVICE)
+            y_bbox = y_bbox.to(DEVICE)
+            y_digit_count = y_digit_count.to(DEVICE)
+
+
             print('Iteration', iteration)
             spair_optim.zero_grad()
-            loss, out_img, z_where = spair_net(batch, iteration)
+            loss, out_img, z_where, z_pres = spair_net(x_image, iteration)
             loss.backward(retain_graph = True)
             spair_optim.step()
 
             # logging stuff
             image_out = out_img[0]
-            image_in = batch[0]
+            image_in = x_image[0]
             combined_image = torch.cat([image_in, image_out], dim=2)
             writer.add_image('SPAIR input_output', combined_image,  iteration)
-            torch.cuda.empty_cache()
+
+            # Log average precision metric every 5 step after 1000 iterations (when trainig_wheel is off)
+            if iteration > 1000 and iteration % 5 == 0: # iteration > 1000 and
+                meanAP = metric.mAP(z_where, z_pres,  y_bbox, y_digit_count)
+                print('Bbox Average Precision:', meanAP.item())
+                writer.add_scalar('accuracy/bbox_average_precision', meanAP, iteration)
+
             print('=================\n\n')
+            torch.cuda.empty_cache()
 
     child_nr = 0
 